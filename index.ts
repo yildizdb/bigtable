d8cfc3ce2c555b7e1aacbabe9d6a04524b3c4625
rjmasikome <rjmasikome@gmail.com>
2018-10-12 13:44:18 +0200

    tightly coupled referenced .d.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+/// <reference path="./lib/declarations/GoogleBigtable.d.ts"/>
 import { BigtableFactory } from "./lib/BigtableFactory";
 export default BigtableFactory;
 export { BigtableFactory }; // faked object export
